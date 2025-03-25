f65363be4d540ded1733c5fd8b4f7e70a0ba1580
ShravanShenoy <shravanshenoy1998@hotmail.com>
2025-03-25 08:53:33 +0530

    Update sql_generator.py

--- sql_generator.py
+++ sql_generator.py
@@ -1,26 +1,31 @@
 import yaml
 import argparse
 
-# Set up argument parser
-parser = argparse.ArgumentParser(description="Generate SQL from YAML file")
-parser.add_argument("--config", type=str, required=True, help="Path to the YAML file")
+def generate_sql(yaml_file):
+    """Generate an SQL query with a CASE WHEN statement based on a YAML config file."""
+    with open(yaml_file, "r") as file:
+        data = yaml.safe_load(file)
 
-# Parse arguments
-args = parser.parse_args()
+    # Start SQL query
+    sql_query = "SELECT date_column,\n  CASE"
 
-# Read YAML file
-with open(args.config, "r") as file:
-    data = yaml.safe_load(file)
+    # Loop through YAML data to generate CASE WHEN statements
+    for entry in data['interest_by_date']:
+        sql_query += f"\n    WHEN date_column BETWEEN '{entry['start_date']}' AND '{entry['end_date']}' THEN {entry['interest']}"
 
-# Start SQL query
-sql_query = "SELECT date_column,\n  CASE"
+    # Add default case and end CASE statement
+    sql_query += "\n    ELSE 0\n  END AS interest_value\nFROM table_name;"
 
-# Loop through YAML data to generate CASE WHEN statements
-for entry in data['interest_by_date']:
-    sql_query += f"\n    WHEN date_column BETWEEN '{entry['start_date']}' AND '{entry['end_date']}' THEN {entry['interest']}"
+    return sql_query
 
-# Add default case and end CASE statement
-sql_query += "\n    ELSE 0\n  END AS interest_value\nFROM table_name;"
+if __name__ == "__main__":
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Generate SQL from YAML file")
+    parser.add_argument("--config", type=str, required=True, help="Path to the YAML file")
 
-# Print the SQL query
-print(sql_query)
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Generate and print SQL
+    sql_query = generate_sql(args.config)
+    print(sql_query)
